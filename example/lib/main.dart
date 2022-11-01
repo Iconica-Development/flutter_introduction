7e88d7238d2770d6da6158fdb5e576ab54abe2d1
Bart Ribbers <bart@iconica.nl>
2022-11-01 08:27:11 +0100

    Add BSD-3-Clause license

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 import 'package:flutter/material.dart';
 import 'package:flutter_introduction/flutter_introduction.dart';
 import 'package:flutter_introduction_shared_preferences/flutter_introduction_shared_preferences.dart';
--- lib/flutter_introduction.dart
+++ lib/flutter_introduction.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 library flutter_introduction;
 
 import 'package:flutter/material.dart';
